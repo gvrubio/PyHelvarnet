eafe3470b114eaf387dd192c2a2ee7a9d0e50d5d
gvrubio <gvidalrubio@gmail.com>
2022-06-23 22:02:47 +0200

    Fixed missing operand on string concatenation

--- pyhelvarnet.py
+++ pyhelvarnet.py
@@ -549,7 +549,7 @@ class HelvarNetClient:
             self.__HLVFADET + fade + "," +\
             "@" + self.clusterID + "." + self.memberID + "." + subnet + "." + device +\
             self.__TERMINATOR
-        print("Recalled Scene " + scene " for device " + device + " on subnet " + subnet + ".")
+        print("Recalled Scene " + scene + " for device " + device + " on subnet " + subnet + ".")
         print("Sent command looks like: " + message)
         self.__SendTCPMessageAndContinue(self.server, self.port, message)
 
